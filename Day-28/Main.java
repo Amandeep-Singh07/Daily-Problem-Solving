9ea22290f4d026ec83367fec3a71dfce633128d9
Amandeep Singh <amandeepronaldo@gmail.com>
2025-03-02 17:11:42 +0530

    importing arrays

---
+++ Day-28/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.Arrays;
+
+class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.nextLine();
+        
+        
+
+        char arr1[] = a.toCharArray();
+        
+        System.out.print(Arrays.toString(arr1));
+    }
+}
+

5e01543dd6dc93c220dcd561757799166e280552
Amandeep Singh <amandeepronaldo@gmail.com>
2025-03-26 21:03:52 +0530

    Valleys and Peaks: Locate the First High Point in an Array

--- Day-56/Main.java
+++ Day-56/Main.java
@@ -31,25 +31,97 @@
 // Output 2 :
 // Peak Element: 8 at index 3
 
-// You are using Java
-public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+import java.util.Scanner;
 
-    int num = sc.nextInt();
-    int arr[] = new int[num];
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    for (int i = 0; i < num; i++) {
-        arr[i] = sc.nextInt();
-    }
+        int num = sc.nextInt();
+        int arr[] = new int[num];
 
-    int flag = 0;
-    int greatest = 0;
-    for (int i = 0; i < num; i++) {
-        if (arr[i] > greatest) {
-            greatest = arr[i];
-            flag = i;
+        for (int i = 0; i < num; i++) {
+            arr[i] = sc.nextInt();
         }
-    }
-    System.out.println("Peak Element: " + greatest + " at index " + flag);
 
+        int flag = 0;
+        int greatest = 0;
+        for (int i = 0; i < num; i++) {
+            if (arr[i] > greatest) {
+                greatest = arr[i];
+                flag = i;
+            }
+        }
+        System.out.println("Peak Element: " + greatest + " at index " + flag);
+
+    }
 }
+
+// another easy question on array
+// Problem Statement
+
+
+
+// Arun is given an array of integers. His task is to find the product of elements at odd positions in the array.
+
+
+
+// Write a program to help Arun that takes an integer N as input, representing the size of the array, followed by N integers representing the elements of the array. The program should then calculate and print the product of elements at odd positions.
+
+// Input format :
+// The first line of input consists of an integer N, representing the number of elements in the array.
+
+// The second line consists of N space-separated integers, representing the elements of the array.
+
+// Output format :
+// The output prints the product of elements at odd positions. Position starts from 1.
+
+
+
+// ﻿Refer to the sample output for formatting specifications.
+
+// Code constraints :
+// The given test cases fall under the following constraints:
+
+// 1 ≤ N ≤ 15
+
+// 0 ≤ array elements ≤ 100
+
+// Sample test cases :
+// Input 1 :
+// 4
+// 4 0 2 1
+// Output 1 :
+// 8
+// Input 2 :
+// 5
+// 1 5 7 9 2
+// Output 2 :
+// 14
+// Input 3 :
+// 5
+// 1 7 0 8 9
+// Output 3 :
+// 0
+
+// import java.util.Scanner;
+
+// class Main {
+//     // You are using Java
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         int num = sc.nextInt();
+//         int arr[] = new int[num];
+
+//         for (int i = 0; i < num; i++) {
+//             arr[i] = sc.nextInt();
+//         }
+//         int product = 1;
+//         for (int i = 0; i < num; i += 2) {
+//             product *= arr[i];
+//         }
+//         System.out.println(product);
+
+//     }
+// }

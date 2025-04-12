ab569187ecc5cddb54e00f539b06dc06351c5e86
Amandeep Singh <amandeepronaldo@gmail.com>
2025-04-12 22:34:56 +0530

    Reading and writing Multiline Input and Output

--- Day-79/Main.java
+++ Day-79/Main.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter lines of text (type 'exit' to quit):");
+        while (true) {
+            String line = scanner.nextLine();
+            if ("exit".equalsIgnoreCase(line))  // this line will ignore the cases like exit ,ExiT,Exit 
+                break;
+            System.out.println("You wrote: " + line);
+        }
+        scanner.close(); // It's good practice to close the Scanner when you're done with it
+        System.out.println("Exiting program.");
+    }
 }

7924b8bf49c79014f8f20bc57caad0caff354dcb
Amandeep Singh <amandeepronaldo@gmail.com>
2025-05-15 00:37:34 +0530

    Pattern printing in cpp

--- Day-110/Main.cpp
+++ Day-110/Main.cpp
@@ -1,1 +1,21 @@
 #include <iostream>
+using namespace std;
+
+void print1(int n){
+    for (int i = 0; i < n;i++){
+        for (int j = 0; j < i;j++){
+            cout << " ";
+        }
+        for (int k = 0; k <2*(n-i)-1;k++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+
+int main(){
+    int n;
+    cin >> n;
+    print1(n);
+}

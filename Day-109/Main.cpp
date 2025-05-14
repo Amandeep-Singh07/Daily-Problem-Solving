e0e58a1442dabe878e51c7ff1580f9d735180fb2
Amandeep Singh <amandeepronaldo@gmail.com>
2025-05-14 23:57:32 +0530

    pattern in cpp

--- Day-109/Main.cpp
+++ Day-109/Main.cpp
@@ -1,1 +1,21 @@
+//  pattern printing...
 #include <iostream>
+using namespace std;
+
+void print1(int n){
+    for (int i = 0; i < n;i++){
+        for (int j = n-i-1;j>0;j--){
+            cout << " ";
+        }
+        for (int k = 0; k <2*i+1;k++){
+            cout << "*";
+        }
+       
+        cout << endl;
+    }
+}
+int main(){
+    int n;
+    cin >> n;
+    print1(n);
+}

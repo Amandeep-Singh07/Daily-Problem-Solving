1870a4adac4f3788897cc5451aff5b90af64762a
Amandeep Singh <amandeepronaldo@gmail.com>
2025-05-16 02:10:25 +0530

    alphabet pattern printing

--- Day-106/Main.cpp
+++ Day-106/Main.cpp
@@ -1,1 +1,21 @@
 #include <iostream>
+using namespace std;
+
+void print1(int n){
+    
+    for (int i = 0; i < n;i++){
+        for (char ch = 'A'; ch <='A'+i;ch++){
+            
+            cout << ch ;
+            cout << " ";
+            
+        }
+            cout << endl;
+    }
+
+}
+int main(){
+    int n;
+    cin >> n;
+    print1(n);
+}

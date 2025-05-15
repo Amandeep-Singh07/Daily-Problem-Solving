7515577060e03c722f76df1d210dbdc4d7d2e251
Amandeep Singh <amandeepronaldo@gmail.com>
2025-05-16 01:52:34 +0530

    Pattern Problem

--- Day-107/Main.cpp
+++ Day-107/Main.cpp
@@ -1,1 +1,20 @@
 #include <iostream>
+using namespace std;
+
+void print1(int n){
+    int k = 1;
+    for (int i = 0; i < n;i++){
+        for (int j = 0; j <=i;j++){
+            cout << k ;
+            cout << " ";
+            k++;
+        }
+            cout << endl;
+    }
+
+}
+int main(){
+    int n;
+    cin >> n;
+    print1(n);
+}

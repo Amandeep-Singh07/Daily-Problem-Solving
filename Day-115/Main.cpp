8d285475a862b73ae9fb5a354f4571ed85759458
Amandeep Singh <amandeepronaldo@gmail.com>
2025-05-17 15:13:05 +0530

    Alphabetical pattern

--- Day-115/Main.cpp
+++ Day-115/Main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 using namespace std;
 
+void print1(int n){
 
+    for (int i = 0; i < n;i++){
+        for (char ch='E'-i; ch<='E';ch++){
+            cout << ch << " ";
+            
+        }
+        cout << endl;
+    }
 
-
+}
 
 int main(){
 
-    
+    int n;
+    cin >> n;
+    print1(n);
 }

a85dbc71cafab4799d2cb49d6a0deef84bf7757c
Amandeep Singh <amandeepronaldo@gmail.com>
2025-05-19 08:29:30 +0530

    CPP STL

--- Day-116/Main.cpp
+++ Day-116/Main.cpp
@@ -18,9 +18,31 @@ void explainPair() {
 
 }
 
+void explainVector() {
+    vector<int> v;
+    v.push_back(1);
+    v.emplace_back(2);
+
+    vector<pair<int, int>> vec;
+    vec.push_back({1, 2});
+    vec.emplace_back(1, 2);
+
+    vector<int> v1(5, 100); // v1 with five 100s
+    vector<int> v2(5);      // v2 with five default-initialized elements (0)
+
+    vector<int> v3(5, 20);
+    vector<int> v4(v3);
+}
+
+
 int main() {
     cout << "Let's explore pairs in C++:" << endl;
     explainPair();
     cout << "End of pair explanation." << endl;
+
+    cout << "Let's explore vectors in C++:" << endl;
+    explainVector();
+    cout << "End of vector explanation." << endl;
+
     return 0;
 }

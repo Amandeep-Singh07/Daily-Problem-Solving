b63b1a6ab0c126e13259563d544ec31db40bb28c
Amandeep Singh <amandeepronaldo@gmail.com>
2025-06-01 17:15:04 +0530

    Day-129

--- Day-129/Main.cpp
+++ Day-129/Main.cpp
@@ -1,1 +1,37 @@
 #include <iostream>
+using namespace std;
+
+void print1(int n){
+    for (int i = 0; i < n;i++){
+        for (int j = n-i-1; j >0;j--){
+            cout << " ";
+        }
+        for (int j = 0; j <= i;j++){
+            cout << "*";
+        }
+        for (int k = 1; k <= i;k++){
+            cout << "*";
+        }
+            cout << endl;
+    }
+
+    for (int i = 0; i < n;i++){
+        for (int j =0; j <i;j++){
+            cout << " ";
+        }
+        for (int j = n-i; j > 0;j--){
+            cout << "*";
+        }
+        for (int j = n-i-1; j > 0;j--){
+            cout << "*";
+        }
+       
+            cout << endl;
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    print1(n);
+}

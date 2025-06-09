eeca0e37513d292407df8f6fc3cfbfdead9d3f21
Amandeep Singh <amandeepronaldo@gmail.com>
2025-06-09 16:22:51 +0530

    Power of Two

--- Day-131/Main.cpp
+++ Day-131/Main.cpp
@@ -1,1 +1,12 @@
 #include <iostream>
+// Time Complexity is O(1).
+class Solution {
+    public:
+        bool isPowerOfTwo(int n) {
+            for(int i=0;i<=30;i++){
+                if(pow(2,i)==n)
+                return true;
+            }
+            return false;
+        }
+    };

a1f804022ae9fa2dcaa76b655a0311f13774286b
Amandeep Singh <amandeepronaldo@gmail.com>
2025-06-09 18:25:00 +0530

    Remove Duplicates

--- Day-126/Main.cpp
+++ Day-126/Main.cpp
@@ -1,1 +1,20 @@
 #include <iostream>
+class Solution {
+    public:
+        int removeDuplicates(vector<int>& nums) {
+            int n=nums.size();
+            int i=0;
+            int j=1;
+    
+            while(j<n){
+            if(nums[i]!=nums[j]){// got a unique element
+                i++;
+                nums[i]=nums[j];
+            }
+            j++;
+            }
+                
+            return i+1;
+    
+        }
+    };

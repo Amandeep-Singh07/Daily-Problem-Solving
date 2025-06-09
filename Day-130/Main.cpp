79a08b03ffd999456f0b574f34f5017e3b94ba56
Amandeep Singh <amandeepronaldo@gmail.com>
2025-06-09 16:24:00 +0530

    Bitwise Complement

--- Day-130/Main.cpp
+++ Day-130/Main.cpp
@@ -1,1 +1,27 @@
 #include <iostream>
+// Time Complexity of O(log(n))
+class Solution {
+    public:
+        int bitwiseComplement(int n) {
+    
+            
+            int temp=n;
+            int mask=0;
+            
+            // remember this edge case
+            if(temp ==0){
+                return 1;
+            }
+    
+            while(temp!=0){
+                
+              mask = (mask<<1) | 1;
+              temp= temp>>1;
+            }
+            
+            int ans=(~n)& mask;
+            return ans;
+    
+            
+        }
+    };

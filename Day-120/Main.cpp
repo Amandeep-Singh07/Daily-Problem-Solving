f408f267a450b37a4ba7fb2942154dfd6b9f8e7f
Amandeep Singh <amandeepronaldo@gmail.com>
2025-06-11 18:18:44 +0530

    Divisor of a number

--- Day-120/Main.cpp
+++ Day-120/Main.cpp
@@ -1,1 +1,19 @@
 #include <iostream>
+class Solution {
+    public:
+        bool isPrime(int n) {
+            if(n<2)return false;
+            if(n==2){
+                return true;
+            }
+            int flag=1;
+            for(int i=2;i*i<=n;i++){
+                if(n%i==0){
+                    return false;
+                }
+            }
+            
+    
+            return true;
+        }
+    };

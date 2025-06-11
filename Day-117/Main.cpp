eb22554aadfffa3568d61d6741462bf6b918cacf
Amandeep Singh <amandeepronaldo@gmail.com>
2025-06-11 17:15:18 +0530

    GCD

--- Day-117/Main.cpp
+++ Day-117/Main.cpp
@@ -1,1 +1,20 @@
 #include<iostream>
+class Solution {
+    public:
+        int GCD(int n1,int n2) {
+            int gcd=1;
+            int smaller=n1;
+            if(n1>n2){
+                smaller=n2;
+            }
+            
+    
+            for(int i=1;i<=smaller;i++){
+                if(n1%i==0 && n2%i==0){
+                    gcd=i;
+                }
+            }
+    
+            return gcd;
+        }
+    };

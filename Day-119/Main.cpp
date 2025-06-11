b05be42dcafb47ac2e825a092bdd0b3384b56321
Amandeep Singh <amandeepronaldo@gmail.com>
2025-06-11 18:07:26 +0530

    Divisor of a number

--- Day-119/Main.cpp
+++ Day-119/Main.cpp
@@ -1,1 +1,14 @@
 #include <iostream>
+
+class Solution {
+    public:
+        vector<int> divisors(int n) {
+            vector<int> arr;
+            for(int i=1;i<=n;i++){
+                if(n%i==0){
+                    arr.push_back(i);
+                }
+            }
+            return arr;
+        }
+    };

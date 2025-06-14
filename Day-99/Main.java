e7d50de9a64268fb65569166adf6220a45ae0887
Amandeep Singh <amandeepronaldo@gmail.com>
2025-06-14 16:21:32 +0530

    1838. Frequency of the Most Frequent Element

--- Day-99/Main.java
+++ Day-99/Main.java
@@ -1,3 +1,21 @@
-public class Main {
+class Solution {
+    public:
+        int maxFrequency(vector<int>& nums, int k) {
+     sort(nums.begin(), nums.end());
+            long long left = 0, total = 0;
+            long long res = 0;
     
-}
+            for (int right = 0; right < nums.size(); ++right) {
+                total += nums[right];
+    
+                while ((long long)nums[right] * (right - left + 1) - total > k) {
+                    total -= nums[left];
+                    left++;
+                }
+    
+                res = max(res, (long long)(right - left + 1));
+            }
+    
+            return (int)res;
+        }
+    };

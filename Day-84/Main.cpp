2ca0149ce743cae5cb50e4acfe6b3ee91dca3024
Amandeep Singh <amandeepronaldo@gmail.com>
2025-07-08 20:56:53 +0530

    31. next permutation

--- Day-84/Main.cpp
+++ Day-84/Main.cpp
@@ -1,11 +1,15 @@
 //----->> Leetcode question number 31 .
+#include <iostream>
+
+int main{
+
+
 class Solution {
     public:
         void nextPermutation(vector<int>& nums) {
             int n = nums.size();
             int i = n - 2;
     
-            // Step 1: Find the first decreasing element from the back
             while (i >= 0 && nums[i] >= nums[i + 1]) {
                 i--;
             }
@@ -20,4 +24,5 @@ class Solution {
     
             reverse(nums.begin() + i + 1, nums.end());
         }
-    };
+    }
+}
